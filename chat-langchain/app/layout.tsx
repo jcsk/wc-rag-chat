1a76bf5deed3c7eb44692d65867fe50af2a1ba4a
geofree <jeffrey@blacksquare.com>
2023-10-22 20:47:20 -0700

    small changes

--- chat-langchain/app/layout.tsx
+++ chat-langchain/app/layout.tsx
@@ -1,27 +1,29 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Chat WineCountry',
-  description: 'Chatbot for Wine Country',
-}
+  title: "Chat WC",
+  description: "Chatbot for WC",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-
   return (
     <html lang="en" className="h-full">
       <body className={`${inter.className} h-full`}>
-        <div className="flex flex-col h-full md:p-8" style={{"background" : "rgb(38, 38, 41)"}}>
+        <div
+          className="flex flex-col h-full md:p-8"
+          style={{ background: "rgb(38, 38, 41)" }}
+        >
           {children}
         </div>
       </body>
     </html>
-  )
+  );
 }
